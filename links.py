d4f0389e843f84919831db239d193450c030c7db
Alex62-Creator <cham62ss@gmail.com>
2025-02-12 21:17:45 +0300

    add lincs and quote

---
+++ links.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "http://quotes.toscrape.com/"
+response = requests.get(url)
+html = response.content
+
+soup = BeautifulSoup(html, "html.parser")
+
+links = soup.find_all("a")
+for link in links:
+    #print(link)
+    print(link.get('href'))
